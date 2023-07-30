a1f296e343c36f7e99068251dc9fb60eba7f654e
MuhammadA-C <phantomganhdi@gmail.com>
2023-07-29 18:50:23 -0700

    Found a Bug
    After adding the code for the equals button to update the screen with the result, the previous values are still present.
    
    So, the old number2 value is present and new values are being appended to it.

--- javascript/main.js
+++ javascript/main.js
@@ -34,10 +34,13 @@ for (let i = 0; i < buttons.btnNumbersNodeList.length; i++) {
     } else if (operatorSelected === null) {
       numberOneSelected += buttons.getBtnNumberText(i);
       screen.updateScreenValue(numberOneSelected);
+
     } else if (numberTwoSelected === null){
       numberTwoSelected = buttons.getBtnNumberText(i);
       screen.updateScreenValue(numberTwoSelected);
+      
     } else {
+      //Bug
       numberTwoSelected += buttons.getBtnNumberText(i);
       screen.updateScreenValue(numberTwoSelected);
     }
@@ -64,9 +67,18 @@ buttons.multiplicationBtn.addEventListener("click", () => {
   operatorSelected = buttons.getBtnText(buttons.multiplicationBtn);
 });
 
+/*
+
+  Bug- Equals method has a bug after pressing it.
+  Number1 and Number2 still has their values, so after clicking the 
+  equals sign the code will go down to the else clause for number two
+  and append the new number entered onto the previous value in number2
+
+
+*/
 buttons.equalsBtn.addEventListener("click",() => {
   console.log(`Num1: ${numberOneSelected}, Num2: ${numberTwoSelected}, Operator: ${operatorSelected}`);
-  console.log(math.operate(operatorSelected, numberOneSelected, numberTwoSelected));
+  screen.updateScreenValue(math.operate(operatorSelected, numberOneSelected, numberTwoSelected));
 });
 
 //I figured reloading the page would be an easy way to reset the calculator
